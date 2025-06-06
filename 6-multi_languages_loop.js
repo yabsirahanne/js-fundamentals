1f30410a6695c87a769d13f553f70438003d0a17
yabsira Hanne <yabsirahanne@gmail.com>
2025-06-06 09:48:33 +0300

    concat

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,9 @@
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (const line of languages) {
-    console.log(line);
+const messages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
 }
