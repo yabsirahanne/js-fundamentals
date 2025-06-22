23335eae8820b3f38cb6ff1523a9e5209de43b4d
yabsira Hanne <yabsirahanne@gmail.com>
2025-06-22 23:57:25 +0300

    ..;

--- 9-add.js
+++ 9-add.js
@@ -1,8 +1,8 @@
-const add = (a, b) => a + b;
+function add(a, b) {
+  console.log(a + b);
+}
 
-const args = process.argv.slice(2);
-const num1 = parseInt(args[0], 10);
-const num2 = parseInt(args[1], 10);
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
 
-console.log(Number.isInteger(num1) && Number.isInteger(num2) ? add(num1, num2) : "Invalid input");
-rev
+add(arg1, arg2);
